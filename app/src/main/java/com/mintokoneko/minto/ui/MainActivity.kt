c737d1ed71b1f64f9e982dd21e16536baf1d7377
Serhii Tymoshenko <serhii.tymoshenko.ua@gmail.com>
2023-11-19 04:29:31 +0100

    Swipeable user photo

--- app/src/main/java/com/mintokoneko/minto/ui/MainActivity.kt
+++ app/src/main/java/com/mintokoneko/minto/ui/MainActivity.kt
@@ -1,24 +1,23 @@
 package com.mintokoneko.minto.ui
 
+import android.annotation.SuppressLint
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.GestureDetector
+import android.view.MotionEvent
 import android.widget.Toast
+import androidx.core.view.GestureDetectorCompat
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.navigation.NavigationBarView
-import com.google.android.material.sidesheet.SideSheetDialog
-import com.mintokoneko.minto.R
 import com.mintokoneko.minto.databinding.ActivityMainBinding
-import com.mintokoneko.minto.databinding.ContentMainBinding
-import com.mintokoneko.minto.ui.bottom_sheet.BottomSheet
+import com.mintokoneko.minto.utils.USER_PHOTO_SWIPE_THRESHOLD
 import com.mintokoneko.minto.utils.getWidth
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var mainContent: ContentMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +25,37 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setupNavigation(this)
+        setSwipeableUser()
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private fun setSwipeableUser() {
+        val gestureDetectorListener = object : GestureDetector.SimpleOnGestureListener() {
+            override fun onDown(e: MotionEvent): Boolean {
+                return true
+            }
+
+            override fun onFling(
+                e1: MotionEvent?,
+                e2: MotionEvent,
+                velocityX: Float,
+                velocityY: Float
+            ): Boolean {
+                e1?.let {
+                    if (e1.y - e2.y > USER_PHOTO_SWIPE_THRESHOLD) {
+                        Toast.makeText(this@MainActivity, "Previous user", Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(this@MainActivity, "Next user", Toast.LENGTH_SHORT).show()
+                    }
+                }
+                return true
+            }
+        }
+        val gestureDetector = GestureDetectorCompat(this, gestureDetectorListener)
+
+        binding.mainContent.appBarLayout.topAppBarUserPhoto.setOnTouchListener { v, event ->
+            gestureDetector.onTouchEvent(event)
+        }
     }
 
     private fun setupNavigation(context: Context) {
@@ -35,20 +65,15 @@ class MainActivity : AppCompatActivity() {
 
         if (getWidth(context) < 600) {
             mainContent.bottomNavigation?.setupWithNavController(navController)
-
         } else {
             mainContent.navigationRail?.setupWithNavController(navController)
         }
 
-        val topAppBar = binding.mainContent.appBarLayout?.topAppBar
+        val topAppBar = mainContent.appBarLayout.topAppBar
         val appBarConfiguration = AppBarConfiguration(navController.graph)
-        topAppBar?.setupWithNavController(navController, appBarConfiguration)
-        topAppBar?.setOnMenuItemClickListener { menuItem ->
+        topAppBar.setupWithNavController(navController, appBarConfiguration)
+        topAppBar.setOnMenuItemClickListener { menuItem ->
             NavigationUI.onNavDestinationSelected(menuItem, navController)  // setup settings nav
-
-//            if (menuItem.itemId == R.id.user_mi) {
-//                showSheet(context)
-//            }
             true
         }
     }
---
+++ app/src/main/java/com/mintokoneko/minto/utils/Constants.kt
@@ -0,0 +1,3 @@
+package com.mintokoneko.minto.utils
+
+const val USER_PHOTO_SWIPE_THRESHOLD = 15
